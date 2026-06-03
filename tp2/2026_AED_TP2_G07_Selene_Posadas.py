cd68f8881762fbceb4cf7e616fee7c0b140ddf4e
Selee-Posadas <215926670+Selee-Posadas@users.noreply.github.com>
2026-06-03 23:03:48 +0000

    agregando pasos

--- tp2/2026_AED_TP2_G07_Selene_Posadas.py
+++ tp2/2026_AED_TP2_G07_Selene_Posadas.py
@@ -10,12 +10,7 @@ datos = open('tratamientos.txt')
 
 #para recorrer el archivo por linea
 for dato in datos:
-    #para recorrer por caracter
-    for car in dato:
-        print(car)
-
-        #aca iria la logica de control
-
+    print(dato)
 
 
 
